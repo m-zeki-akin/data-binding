9426996a4e84e6f775961b8d6a8d0abfa0fa7356
M. Zeki Akin <mzakin@netas.com.tr>
2020-03-08 15:17:37 +0300

    m

---
+++ src/main/java/com/mavendemo/databinding/Driver.java
@@ -0,0 +1,34 @@
+package com.mavendemo.databinding;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+
+			// Create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// Read json file and convert to POJO
+			Student student = mapper.readValue(new File("src/main/resources/data/student-sample.json"), Student.class);
+
+			System.out.println("First name: " + student.getFirstName());
+			System.out.println("Last name: " + student.getLastName());
+			System.out.println("Active Status: " + student.isActive());
+
+		} catch (FileNotFoundException e) {
+			System.out.println(
+					"File: " + new File("src/main/resources/data/student-sample.json").getAbsolutePath() + "not found");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
